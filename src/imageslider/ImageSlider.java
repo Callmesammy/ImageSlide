a9c2267d1b0f1fb87f384468b053121308d12741
user <user@DESKTOP-LKTQ0L2>
2022-08-16 18:36:07 +0100

    Management_Dashboard

---
+++ src/imageslider/ImageSlider.java
@@ -0,0 +1,17 @@
+
+package imageslider;
+
+/**
+ *
+ * @author user
+ */
+public class ImageSlider {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
